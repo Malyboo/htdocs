c0c177eeb1a22639d86b52ba5affcbdb7f9d7529
Malyboo <nandoluh1990@gmail.com>
2025-09-10 17:27:06 -0300

    90% finalizado

--- sitema/listaVenda.php
+++ sitema/listaVenda.php
@@ -128,7 +128,7 @@
     </div>
 
 
-
+  <a href="./principal.php" class="btn btn-sm btn-success" style="width: 80px"> Voltar </a>
   </div>
   
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -139,7 +139,6 @@
     crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-  <script src="script.js"></script>
 
   <script>
     //se tiver alteração no campo região, dispara essa função
@@ -159,7 +158,17 @@
       function(data) { $('#ponto_focal_id').html(data); });
     });
 
-    var tabela = $('#tabela').DataTable();
+   
+    var tabela = $('#tabela').DataTable({
+      dom: 'Bfrtip',
+      buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'],
+        responsive: true,
+        language: {
+        url: 'https://cdn.datatables.net/plug-ins/2.3.2/i18n/pt-BR.json',
+    },
+    });
+  
 
       $('#regiao_id').on('change', function(){
         var texto = $('#regiao_id option:selected').text();
@@ -182,9 +191,28 @@
     });
 
     </script>
-    <script src="script.js"></script>
+    
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 
-  
+  <style>
+    button{
+      background-color:#007fff;
+      margin-bottom: 15px;
+      margin-right: 8px;
+      color: white;
+      padding: 8px;
+      border:none;
+      border-radius: 8px;
+    }
+    #tabela{
+      margin-top: 8;
+    }
+  </style>
 
 </body>
 
